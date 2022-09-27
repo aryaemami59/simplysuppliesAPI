6659ca0eb99457b3e42fac741900767b25484aa0
Arya Emami <aryaemami59@yahoo.com>
2022-09-26 19:54:34 -0500

    Updates

---
+++ Item.d.ts
@@ -0,0 +1,18 @@
+import {
+  itemObjType,
+  addedStateType,
+  errMsgType,
+  itemNameType,
+  itemNumberType,
+  itemStateType,
+  itemsInItemStateType,
+  modifiedItemObjType,
+  navNameType,
+  navObjType,
+  vendorNameType,
+  vendorsInAddedStateType,
+  vendorsObjType,
+} from "./items";
+
+declare const Item: itemObjType;
+export default Item;
