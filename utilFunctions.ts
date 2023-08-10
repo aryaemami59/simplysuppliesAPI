cecb217d1883e280dcef8353d93a2a6d9d1ee382
Arya Emami <aryaemami59@yahoo.com>
2023-08-10 15:25:47 -0500

    Updates

--- utilFunctions.ts
+++
@@ -1,8 +0,0 @@
-import { Categories } from "./items";
-import { Items } from "./looseTypes";
-
-export const getItemsInOneCategory = (
-  category: keyof Categories,
-  categories: Categories,
-  items: Items
-) => categories[category].itemIds.map(e => items.find(({ id }) => id === e));
